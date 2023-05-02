135dabd7fb36f5b4e779743fbba0057f247dae7e
EnricoTrizio <enricotrizio@gmail.com>
2023-05-02 11:28:50 +0200

    DeepTDA with KL divergence, first commit

---
+++ mlcolvar/core/loss/kl_tda_loss.py
@@ -0,0 +1,89 @@
+import torch
+from warnings import warn
+
+__all__ = ["KL_TDA_loss"]
+
+def KL_TDA_loss(H : torch.tensor,
+            labels : torch.tensor,
+            n_states : int,
+            target_centers : list or torch.tensor,
+            target_sigmas : list or torch.tensor) -> torch.tensor:
+    """
+    Compute a loss function as the distance from a simple Gaussian target distribution.
+    
+    Parameters
+    ----------
+    H : torch.tensor
+        Output of the NN
+    labels : torch.tensor
+        Labels of the dataset
+    n_states : int
+        Number of states in the target
+    target_centers : list or torch.tensor
+        Centers of the Gaussian targets
+        Shape: (n_states, n_cvs)
+    target_sigmas : list or torch.tensor
+        Standard deviations of the Gaussian targets
+        Shape: (n_states, n_cvs)
+    alfa : float, optional
+        Centers_loss component prefactor, by default 1
+    beta : float, optional
+        Sigmas loss compontent prefactor, by default 100
+
+    Returns
+    -------
+    torch.tensor
+        Total loss, centers loss, sigmas loss
+    """
+    if not isinstance(target_centers,torch.Tensor):
+        target_centers = torch.Tensor(target_centers)
+    if not isinstance(target_sigmas,torch.Tensor):
+        target_sigmas = torch.Tensor(target_sigmas)
+    
+    device = H.device
+    target_centers = target_centers.to(device)
+    target_sigmas = target_sigmas.to(device) 
+
+    # min_max = torch.zeros_like(target_centers).unsqueeze(0)
+    # min_max = torch.vstack((min_max, min_max))
+    # n_sigma = target_centers / target_sigmas
+    # min_max[:, 0] = target_centers - n_sigma*target_sigmas
+    # min_max[:, 1] = target_centers + n_sigma*target_sigmas      
+
+    target_distributions = torch.zeros((n_states, 100), device = device)
+    for i in range(n_states): # TODO fix for more dimensions
+        x = torch.linspace(float((target_centers[i]-5*target_sigmas[i]).cpu().numpy()), float((target_centers[i]+5*target_sigmas[i]).cpu().numpy()), 100, device=device)
+        target_distributions[i] = sym_func(x, target_centers[i], target_sigmas[i])
+
+    loss = torch.zeros(n_states)
+    for i in range(n_states):
+        # check which elements belong to class i
+        if not torch.nonzero(labels == i).any():
+            raise ValueError(f'State {i} was not represented in this batch! Either use bigger batch_size or a more equilibrated dataset composition!')
+        else:
+            H_red = H[torch.nonzero(labels == i, as_tuple=True)]
+            mu = torch.mean(H_red, 0)
+            sigma = torch.std(H_red, 0)
+        
+            # x = torch.linspace((mu-4*target_sigmas[i]).numpy(), (mu+4*target_sigmas[i]).numpy(), 100)
+            # target_distributions[i] = sym_func(x, mu, target_sigmas[i])
+            out = easy_KDE(H_red, [float((target_centers[i]-5*target_sigmas[i]).detach().cpu().numpy()), float((target_centers[i]+5*target_sigmas[i]).detach().cpu().numpy())], 100)
+            #out = easy_KDE(H_red, [float((mu-10*target_sigmas[i]).detach().numpy()), float((mu+10*target_sigmas[i]).detach().numpy())], 100)
+            out = out / torch.sum(out)
+            out = out / torch.max(out) * torch.max(target_distributions[i])
+            loss[i] = torch.sum(torch.pow(torch.kl_div(torch.log(out), target_distributions[i]) , 2)) + torch.pow((mu-target_centers[i]),2) + torch.pow((sigma-target_sigmas[i]),2) 
+    loss = torch.sum(loss)
+    return loss
+
+
+def sym_func(x, centers, sigma):
+    return torch.exp(- torch.div(torch.pow(x-centers, 2), 2*torch.pow(sigma,2) ))
+
+def easy_KDE(x, min_max, n):
+    centers = torch.linspace(min_max[0], min_max[1], n, device=x.device)
+    sigma = (centers[1] - centers[0]) * 5
+    out = torch.sum(sym_func(x, centers, sigma), dim=0)
+    return out
+
+
+
---
+++ mlcolvar/cvs/supervised/kl_deeptda_cv.py
@@ -0,0 +1,151 @@
+import torch 
+import pytorch_lightning as pl
+from mlcolvar.cvs import BaseCV
+from mlcolvar.core import FeedForward, Normalization
+from mlcolvar.core.loss.kl_tda_loss import KL_TDA_loss
+from mlcolvar.data import DictionaryDataModule
+
+__all__ = ["KL_DeepTDA_CV"]
+
+class KL_DeepTDA_CV(BaseCV, pl.LightningModule):
+    """
+    Define Deep Targeted Discriminant Analysis (Deep-TDA) CV.
+    Combine the inputs with a neural-network and optimize it in a way such that the data are distributed accordingly to a target distribution.
+    """
+
+    BLOCKS = ['normIn', 'nn']
+
+    # TODO n_states optional?
+    def __init__(self,
+                n_states : int,
+                n_cvs : int,
+                target_centers : list, 
+                target_sigmas : list, 
+                layers : list,
+                KLD_reg : str = None, 
+                options : dict = {}, 
+                **kwargs):
+        """
+        Define Deep Targeted Discriminant Analysis (Deep-TDA) CV.
+
+        Parameters
+        ----------
+        n_states : int
+            Number of states for the training
+        n_cvs : int
+            Numnber of collective variables to be trained
+        target_centers : list
+            Centers of the Gaussian targets
+        target_sigmas : list
+            Standard deviations of the Gaussian targets
+        layers : list
+            Number of neurons per layer
+        KLD_reg : bool,
+            Add a Kullback-Leibler divergence term to the loss to actually enforce the gaussian distribution, by default False.
+            If True, use more epochs for training as it slows down the learning procedure
+        options : dict[str, Any], optional
+            Options for the building blocks of the model, by default {}.
+            Available blocks: ['normIn', 'nn'].
+            Set 'block_name' = None or False to turn off that block
+        """
+
+        super().__init__(in_features=layers[0], out_features=layers[-1], **kwargs)
+
+        options = self.sanitize_options(options)
+        
+        self.n_states = n_states
+        if self.out_features != n_cvs:
+            raise ValueError("Number of neurons of last layer should match the number of CVs!")
+        
+        # check size  and type of targets
+        if not isinstance(target_centers,torch.Tensor):
+            target_centers = torch.Tensor(target_centers)
+        if not isinstance(target_sigmas,torch.Tensor):
+            target_sigmas = torch.Tensor(target_sigmas)
+
+        if target_centers.shape != target_sigmas.shape:
+            raise ValueError(f"Size of target_centers and target_sigmas should be the same!")
+        if n_states != target_centers.shape[0]:
+            raise ValueError(f"Size of target_centers at dimension 0 should match the number of states! Expected {n_states} found {target_centers.shape[0]}")
+        if len(target_centers.shape) == 2:
+            if n_cvs != target_centers.shape[1]:
+                raise ValueError((f"Size of target_centers at dimension 1 should match the number of cvs! Expected {n_cvs} found {target_centers.shape[1]}"))
+
+        # Initialize normIn
+        o = 'normIn'
+        if ( not options[o] ) and (options[o] is not None):
+            self.normIn = Normalization(self.in_features,**options[o])
+
+        # initialize NN
+        o = 'nn'
+        self.nn = FeedForward(layers, **options[o])
+    
+        self.loss_options = {'n_states': n_states,
+                             'target_centers': target_centers,
+                             'target_sigmas':  target_sigmas                     
+                            }
+
+    # TODO change to have standard signature?
+    def loss_function(self, input, labels, **kwargs):
+        loss = KL_TDA_loss(input, labels, **kwargs)
+        return loss
+
+    def training_step(self, train_batch, batch_idx):
+        options = self.loss_options.copy()
+        # =================get data===================
+        x = train_batch['data']
+        labels = train_batch['labels']
+        # =================forward====================
+        z = self.forward_cv(x)
+        # ===================loss=====================
+        loss = self.loss_function(z, labels, **options)
+        # ====================log=====================+
+        name = 'train' if self.training else 'valid'
+        self.log(f'{name}_loss', loss, on_epoch=True,prog_bar=True, )
+        return loss
+
+# TODO signature of tests?
+import numpy as np
+def test_deeptda_cv():
+    from mlcvs.data import DictionaryDataset
+
+    for states_and_cvs in [  [1, 1]]:
+        # get the number of states and cvs for the test run
+        n_states = states_and_cvs[0]
+        n_cvs = states_and_cvs[1]
+        
+        in_features, out_features = 2, n_cvs 
+        layers = [in_features, 4, 2, out_features]
+        target_centers = np.random.randn(n_states, n_cvs)
+        target_sigmas = np.random.randn(n_states, n_cvs)
+
+        # test initialize via dictionary
+        options= { 'nn' : { 'activation' : 'relu' } }
+
+        model = KL_DeepTDA_CV(n_states = n_states, n_cvs = n_cvs, target_centers = target_centers, target_sigmas = target_sigmas, layers = layers, options=options)
+        
+        print('----------')
+        print(model)
+
+        # create dataset
+        samples = 100
+        X = torch.randn((samples * n_states, 2))
+
+        # create labels
+        y = torch.zeros(X.shape[0])
+        for i in range(1, n_states):
+            y[samples*i:] += 1
+        
+        dataset = DictionaryDataset({'data': X, 'labels' : y})
+        datamodule = DictionaryDataModule(dataset,lengths=[0.75,0.2,0.05], batch_size=samples)        
+        # train model
+        trainer = pl.Trainer(accelerator='cpu', max_epochs=2, logger=None, enable_checkpointing=False)
+        trainer.fit( model, datamodule )
+
+        # trace model
+        traced_model = model.to_torchscript(file_path=None, method='trace', example_inputs=X[0])
+        model.eval()
+        assert torch.allclose(model(X),traced_model(X))
+
+if __name__ == "__main__":
+    test_deeptda_cv()
